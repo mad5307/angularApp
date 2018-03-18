a43b699349d62d507ba8a602a5f7abecee07c118
Marco DeLeonardis <deleonardism@itg.com>
2018-03-18 16:23:25 -0400

    Created views

---
+++ js/app.js
@@ -0,0 +1,37 @@
+	// create the module and name it app
+	var app = angular.module('app', ['ngRoute']);
+
+	// configure our routes
+	app.config(function($routeProvider) {
+		$routeProvider
+
+			// route for the home page
+			.when('/home', {
+				templateUrl : 'views/home.html',
+				controller  : 'mainController'
+			})
+
+			// route for the about page
+			.when('/about', {
+				templateUrl : 'views/about.html',
+				controller  : 'aboutController'
+			})
+
+			// route for the contact page   
+			.when('/contact', {
+				templateUrl : 'views/contact.html',
+				controller  : 'contactController'
+            })
+            .otherwise({
+				templateUrl : 'views/home.html',
+				controller  : 'mainController'
+            });
+	});
+
+	app.controller('aboutController', function($scope) {
+		$scope.message = 'Look! I am an about page.';
+	});
+
+	app.controller('contactController', function($scope) {
+		$scope.message = 'Contact us! JK. This is just a demo.';
+	});
